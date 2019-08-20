c4fdacb94c233db0d4d90f5b53c644368c21c8a9
APCS3rd <APCS3rd@MHS-LT9-34>
2019-08-20 10:56:02 -0700

    Completed Hello World Project.

---
+++ HelloWorldKeefer/src/keeferhelloworld.java
@@ -0,0 +1,11 @@
+//Keefer Yip 8/19/19 3rd Period
+//This program prints "Hello World" to the console
+public class keeferhelloworld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+
+	}
+
+}
