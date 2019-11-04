96944ee182d2e07c2c044d533730e4d5ba94dc4b
APCS3rd <APCS3rd@MHS-LT9-34>
2019-11-04 10:48:45 -0800

    array lab

---
+++ Arrays Lab (not the Mini one) 3rd period/src/ArraysLab3.java
@@ -0,0 +1,17 @@
+
+public class ArraysLab3 {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+	
+	public static int[] sum (int[] arr1, int[] arr2) {
+	int[] sumArray = new int[arr1.length];
+	//process the arrays here
+	return sumArray;
+	
+	}
+	
+}
