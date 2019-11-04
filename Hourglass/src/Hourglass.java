25949a4430e29726acdfc9319ee4b532f74fde32
APCS3rd <APCS3rd@MHS-LT9-34>
2019-11-04 10:07:26 -0800

    fraccalc

--- Hourglass/src/Hourglass.java
+++ Hourglass/src/Hourglass.java
@@ -1,25 +1,65 @@
 //Keefer Yip October 8 2019 APCS 3rd
 //Hourglass uses for loops to print an easily modifiable hourglass figure
+import java.util.Scanner;
+
 public class Hourglass {
-	int size = 10;
-	public static void main(String[] args) {
-		System.out.print("|");
-		for(int i = 0; i < 10; i++) {
-			System.out.print("\"");
+    public static void main(String[] args) {
+        // Create new Scanner Object and print welcome message
+        Scanner scanner = new Scanner(System.in);
+   
+        System.out.println("Enter the width of the hourglass ");
+        int width = scanner.nextInt();
+        if (width > 300) {
+            scanner.nextLine();
+            String sure = scanner.nextLine();
+            if (sure.equals("yes!")) {
+                // Build and print the hour glass with the given width
+                buildHGlass(width);
+            }
+        } else {
+            buildHGlass(width);
+        }
+    }
+
+    public static void buildHGlass(int width) {
+        buildTop(width);
+        repeatThis(width - 1, " ");
+        System.out.println("|   |"); 
+        buildBottom(width);
+    }
+
+    public static void buildTop(int width){
+        System.out.print("\n\n| ");
+        repeatThis(width, "\" ");
+        System.out.println("|");
+
+        for (int row = 1; row <= (width - 2) / 2; row++) {
+            repeatThis(row, "  ");
+            System.out.print("\\ ");
+            repeatThis(-2*row+width, ": ");
+            System.out.println("/");
+        }
+    }
+
+    public static void buildBottom(int width) {
+        for (int row = (width - 2) / 2; row >= 1; row--) {
+            repeatThis(row, "  ");
+            System.out.print("/ ");
+            repeatThis(-2*row+width, ": ");
+            System.out.println("\\ ");
+        }
+
+        System.out.print("| ");
+        repeatThis(width, "\" ");
+        System.out.println("|");
+    }
+
+    public static void repeatThis(int times, String character) {
+        for (int i = 0; i < times; i++)
+            System.out.print(character);
+    			}
 		}
-			System.out.println("|");
-		for (int row = 1 ; row <= 4; row++) {
-			for (int space = 1; space <= row; space++) {
-				System.out.print(" ");
-			}
-			System.out.print("\\");
-			for (int colon = 1; colon <= -2 * row + 10; colon++) {
-				System.out.print(":");
-			}
-			System.out.println("/");
-			
-				
-			}
-		}
-	}
+
+
+
 
