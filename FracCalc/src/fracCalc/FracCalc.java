67b24f52c18a30115185396626e7c656d4b4ca58
Keefer Yip <Keefer Yip@DESKTOP-2T8N701.attlocal.net>
2019-11-04 22:19:52 -0800

    frackle cackle

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,11 +1,27 @@
+//Keefer Yip
+//FracCalc Checkpoint 1 - November 2019
+
 package fracCalc;
+
+import java.util.*;
 
 public class FracCalc {
 
-    public static void main(String[] args) 
-    {
-    	
-        // TODO: Read the input from the user and call produceAnswer with an equation
+    	 public static void main(String[] args) 
+    	    {
+    	        Scanner scanner = new Scanner(System.in);
+    	        String quit = " ";        
+    	        String equation = " ";
+    	        System.out.println("Welcome to FracCalc");
+    	        System.out.println("~ Checkpoint 1 ~");
+    	        while(!quit.equalsIgnoreCase("quit")){
+    	            System.out.println("What is your equation?");
+    	            equation = scanner.nextLine();
+    	            System.out.println(produceAnswer(equation));       
+    	            System.out.println("Do you wish to continue? ( Enter \"quit\" to stop)");
+    	            quit = scanner.nextLine();     }
+    	    scanner.close();
+ // TODO: Read the input from the user and call produceAnswer with an equation
 
     }
     
@@ -20,8 +36,9 @@ public class FracCalc {
     public static String produceAnswer(String input)
     { 
         // TODO: Implement this function to produce the solution to the input
-        
-        return "";
+    	String[] frac = input.split(" ");
+        return frac[2];
+
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
